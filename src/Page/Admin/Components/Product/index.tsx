6d68e49fb674ecc3727251783848808ec834d2b0
thanglong2807 <long2323123@gmail.com>
2023-07-07 16:42:20 +0700

    h

--- src/Page/Admin/Components/Product/index.tsx
+++ src/Page/Admin/Components/Product/index.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+interface ProductData {
+  id: number;
+  idProduct: string;
+  imgProduct: string;
+  titleProduct: string;
+  titleItem: string;
+  titlePrice: string;
+}
 const Product: React.FC = () => {
-  return <div className="admin--content">Product</div>;
+  const [data, setData] = useState<ProductData[]>([]);
+  const getData = () => {
+    fetch("https://649be5960480757192371734.mockapi.io/product", {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // handle error
+      })
+      .then((tasks: ProductData[]) => {
+        setData(tasks);
+      })
+      .catch((error) => {
+        // handle error
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="admin--content">
+      <h1>Danh sách sản phẩm</h1>
+
+      <table className="table" cellSpacing={20}>
+        <tr>
+          <td>#</td>
+          <td>Tên sản phẩm</td>
+          <td>Loại sản phẩm</td>
+          <td>Giá tiền</td>
+          <td>Ảnh sản phẩm</td>
+          <td>Action</td>
+        </tr>
+        {data.map((item, i) => (
+          <tr key={item.idProduct}>
+            <td>{i + 1}</td>
+            <td>{item.titleProduct}</td>
+            <td>{item.titleItem}</td>
+            <td>{item.titlePrice}</td>
+            <td>
+              <img className="image_product" src={item.imgProduct} alt="" />
+            </td>
+            <td>
+              <button>Thêm sản phẩm</button>
+              <button>xóa sản phẩm</button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 };
 
 export default Product;
--- src/Page/Admin/index.tsx
+++ src/Page/Admin/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.css";
 import Content from "./Components/Content";
-import Header from "../../Layouts/Header";
 import Sidebar from "./Components/Sidebar";
 import Product from "./Components/Product";
 const Admin: React.FC = () => {
